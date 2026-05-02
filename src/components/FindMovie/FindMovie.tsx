5adc0156bee272c8f92295b92d7db1b6ca928eb7
Serhii Shimko <shimkosergij2010@gmail.com>
2026-05-02 14:57:53 +0300

    completed the task

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import './FindMovie.scss';
+import classNames from 'classnames';
+import { getMovie } from '../../api';
+import { MovieCard } from '../MovieCard';
+import { ResponseError } from '../../types/ReponseError';
+import { MovieData } from '../../types/MovieData';
+import { Movie } from '../../types/Movie';
 
-export const FindMovie: React.FC = () => {
+type Props = {
+  setMovies: (movies: Movie[]) => void;
+  movies: Movie[];
+};
+
+export const FindMovie: React.FC<Props> = ({ setMovies, movies }) => {
+  const [value, setValue] = useState('');
+  const [loading, isLoading] = useState(false);
+  const [showError, setShowError] = useState<ResponseError | null>(null);
+  const [movie, setMovie] = useState<Movie | null>(null);
+
+  const findMovie = useCallback((newValue: string) => {
+    getMovie(newValue).then((response: MovieData | ResponseError) => {
+      isLoading(false);
+      if (response && 'imdbID' in response) {
+        const validMovie: Movie = {
+          title: response.Title,
+          description: response.Plot,
+          imgUrl:
+            response.Poster === 'N/A'
+              ? 'https://via.placeholder.com/360x270.png?text=no%20preview'
+              : response.Poster,
+          imdbUrl: `https://www.imdb.com/title/${response.imdbID}`,
+          imdbId: response.imdbID,
+        };
+
+        setMovie(validMovie);
+      } else {
+        setShowError(response);
+      }
+    });
+  }, []);
+
   return (
     <>
-      <form className="find-movie">
+      <form
+        className="find-movie"
+        onSubmit={e => {
+          e.preventDefault();
+          findMovie(value);
+          isLoading(true);
+        }}
+      >
         <div className="field">
           <label className="label" htmlFor="movie-title">
             Movie title
@@ -16,13 +61,22 @@ export const FindMovie: React.FC = () => {
               type="text"
               id="movie-title"
               placeholder="Enter a title to search"
-              className="input is-danger"
+              className={classNames('input', {
+                'is-danger': showError,
+              })}
+              value={value}
+              onChange={e => {
+                setValue(e.target.value);
+                setShowError(null);
+              }}
             />
           </div>
 
-          <p className="help is-danger" data-cy="errorMessage">
-            Can&apos;t find a movie with such a title
-          </p>
+          {showError && (
+            <p className="help is-danger" data-cy="errorMessage">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
         </div>
 
         <div className="field is-grouped">
@@ -30,28 +84,49 @@ export const FindMovie: React.FC = () => {
             <button
               data-cy="searchButton"
               type="submit"
-              className="button is-light"
+              disabled={value ? false : true}
+              className={classNames('button is-light', {
+                'is-loading': loading,
+              })}
             >
-              Find a movie
+              {movie ? 'Search again' : 'Find a movie'}
             </button>
           </div>
 
-          <div className="control">
-            <button
-              data-cy="addButton"
-              type="button"
-              className="button is-primary"
-            >
-              Add to the list
-            </button>
-          </div>
+          {movie && (
+            <div className="control">
+              <button
+                data-cy="addButton"
+                type="button"
+                className="button is-primary"
+                onClick={() => {
+                  const audit: Movie | undefined = movies.find(
+                    oldMovie => oldMovie.imdbId === movie.imdbId,
+                  );
+
+                  if (!audit) {
+                    const newMovies = [...movies, movie];
+
+                    setMovies(newMovies);
+                  }
+
+                  setValue('');
+                  setMovie(null);
+                }}
+              >
+                Add to the list
+              </button>
+            </div>
+          )}
         </div>
       </form>
 
-      <div className="container" data-cy="previewContainer">
-        <h2 className="title">Preview</h2>
-        {/* <MovieCard movie={movie} /> */}
-      </div>
+      {movie && (
+        <div className="container" data-cy="previewContainer">
+          <h2 className="title">Preview</h2>
+          <MovieCard movie={movie} />
+        </div>
+      )}
     </>
   );
 };
